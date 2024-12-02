890050296a0775de0b02adbb746a395525540fe2
alireza013013 <alireza.abdollahi19998@gmail.com>
2024-12-02 20:19:53 +0330

    first commit

---
+++ app.vue
@@ -0,0 +1,18 @@
+<template>
+  <div
+    style="background-color: black;display: flex;flex-direction: column;align-items: center ;width: 100%;height: 100vh;">
+    <h1 style="color: white;">Welcome To nuxt</h1>
+    <h3 style="color: white;">Test Vidoe Iphone Site</h3>
+    <video controls autoplay muted playsinline width="640" height="360">
+      <source src="https://boomilia.com/media/media/78b2c406497b444ea3e0beada80f98c7.mp4" type="video/mp4" />
+      مرورگر شما از ویدیو پشتیبانی نمی‌کند.
+    </video>
+  </div>
+</template>
+
+
+<style>
+body {
+  margin: 0;
+}
+</style>
