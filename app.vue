315d5ed66cbbac895c09bd5278d17a9e64d47d4a
alireza013013 <alireza.abdollahi19998@gmail.com>
2024-12-02 20:59:09 +0330

    add another video

--- app.vue
+++ app.vue
@@ -4,6 +4,14 @@
     <h1 style="color: white;">Welcome To nuxt</h1>
     <h3 style="color: white;">Test Vidoe Iphone Site</h3>
     <video controls autoplay muted playsinline width="640" height="360">
+      <source
+        src="https://avtshare01.rz.tu-ilmenau.de/avt-vqdb-uhd-1/test_1/segments/bigbuck_bunny_8bit_200kbps_360p_60.0fps_h264.mp4"
+        type="video/mp4" />
+      مرورگر شما از ویدیو پشتیبانی نمی‌کند.
+    </video>
+
+
+    <video controls autoplay muted playsinline width="640" height="360">
       <source src="https://boomilia.com/media/media/78b2c406497b444ea3e0beada80f98c7.mp4" type="video/mp4" />
       مرورگر شما از ویدیو پشتیبانی نمی‌کند.
     </video>
